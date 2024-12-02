0e1923a6ca746abb733715c9eadd0af310658d5c
Fabio Pardo <fpardo1023@gmail.com>
2024-12-02 17:38:08 -0500

    Display Email if user is logged in Homepage

--- handlers/home.go
+++ handlers/home.go
@@ -1,11 +1,18 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/markbates/goth"
 )
 
 func HomeHandler(c *gin.Context) {
-	c.String(http.StatusOK, "Welcome! Go to /auth/google to log in with Google.")
+	gUser, _ := c.Get("user")
+	if gUser, ok := gUser.(goth.User); ok {
+		c.String(http.StatusOK, fmt.Sprintf("Welcome %v", gUser.Email))
+	} else {
+		c.String(http.StatusOK, "Welcome! Go to /auth/google to log in with Google.")
+	}
 }
